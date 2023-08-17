d790cd4bc72f931be51c93380277ebffed80b46d
Avinay kumar <avinay_23@yahoo.co.in>
2023-08-17 14:48:22 +0530

    First commit
    First commit

---
+++ Weather App/Models/WeatherModel.swift
@@ -0,0 +1,69 @@
+//
+//  WeatherModel.swift
+//  Weather App
+//
+//  Created by Admin on 17/08/23.
+//
+
+import Foundation
+
+// MARK: - WeatherModel
+struct WeatherModel: Codable {
+    
+    var coord: Coord?
+    var weather: [Weather]?
+    var base: String?
+    var main: Main?
+    var visibility: Int?
+    var wind: Wind?
+    var clouds: Clouds?
+    var dt: Double?
+    var sys: Sys?
+    var timezone: Double?
+    var id: Int?
+    var name: String?
+    var cod: Int?
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    var all: Int?
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    var lon, lat: Double?
+}
+
+// MARK: - Main
+struct Main: Codable {
+    var temp, feelsLike, tempMin, tempMax: Double?
+    var pressure, humidity: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    var type, id: Int?
+    var country: String?
+    var sunrise, sunset: Int?
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    var id: Int?
+    var main, description, icon: String?
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    var speed: Double?
+    var deg: Int?
+}
